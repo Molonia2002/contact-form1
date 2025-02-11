7c6efcdb5d4ceb100849cc8e2209a06cab263472
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-11 01:52:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import contactForm from "./ContactForm";
-import "./styles.css";
+import ContactForm from "./ContactForm";
 
 function App() {
-	return (
-		<div> 
-			<h1>Customer Support</h1> 
-			<ContactForm/> 
-		</div> 
-	);
+  return (
+    <div>
+      <h1>Customer Support</h1>
+      <ContactForm />
+    </div>
+  );
 }
 
 export default App;
