1057b4e9c3679a487dfb3806924a7863fe036731
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-15 01:15:34 +0100

    Update SelectTicket.js

--- src/pages/SelectTicket.js
+++ src/pages/SelectTicket.js
@@ -1,1 +1,24 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TicketSelection from "../components/TicketSelection";
+import Button from "../components/Button";
 
+const SelectTicket = () => {
+  const [ticketType, setTicketType] = useState("");
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    localStorage.setItem("ticketType", ticketType);
+    navigate("/attendee");
+  };
+
+  return (
+    <div className="text-center">
+      <h2 className="text-white">Select Your Ticket</h2>
+      <TicketSelection setTicketType={setTicketType} />
+      <Button text="Next" onClick={handleNext} disabled={!ticketType} />
+    </div>
+  );
+};
+
+export default SelectTicket;
