0ae2d0ade3139b286d71ed249928eb1eb88faa2e
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-15 01:13:31 +0100

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,1 +1,18 @@
+import React from "react";
 
+const Input = ({ label, type, value, onChange }) => {
+  return (
+    <div className="mb-4">
+      <label className="block text-white">{label}</label>
+      <input
+        className="w-full p-2 border border-gray-300 rounded"
+        type={type}
+        value={value}
+        onChange={onChange}
+        required
+      />
+    </div>
+  );
+};
+
+export default Input;
