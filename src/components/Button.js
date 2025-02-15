60c43f69b8b0d737a6995e7f889a46550cf15a46
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-15 01:12:40 +0100

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+const Button = ({ text, onClick, disabled }) => {
+  return (
+    <button
+      className="bg-blue-500 text-white py-2 px-4 rounded mt-2"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
