4d070ed8d6b3aad57f0f420cc9c4fec5578c8405
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-15 01:21:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import ContactForm from "./ContactForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import TicketSelection from "./pages/TicketSelection";
+import AttendeeDetails from "./pages/AttendeeDetails";
+import TicketReady from "./pages/TicketReady";
 
 function App() {
   return (
-    <div>
-      <h1>Customer Support</h1>
-      <ContactForm />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="min-h-screen bg-[#0D1B2A] text-white">
+        <Routes>
+          <Route path="/" element={<TicketSelection />} />
+          <Route path="/attendee-details" element={<AttendeeDetails />} />
+          <Route path="/ticket-ready" element={<TicketReady />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
